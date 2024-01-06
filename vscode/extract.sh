5751eef30c5f2a1046e46f8941c1ad149bdc8b22
Stanislav Jakuschevskij <stanislav.jakuschevskij@at.ibm.com>
2024-01-06 16:24:18 +0100

    chores: move Brewfile to dedicated dir

--- vscode/extract.sh
+++ vscode/extract.sh
@@ -1,4 +1,4 @@
-echo "copy vscode settings, keymaps and enabled extensions to: $(pwd)"
+echo "copy vscode settings, keymaps extensions to: $(pwd)"
 cp -v ~/Library/Application\ Support/Code/User/settings.json .
 cp -v ~/Library/Application\ Support/Code/User/keybindings.json .
 code --list-extensions --show-versions > extensions.txt
